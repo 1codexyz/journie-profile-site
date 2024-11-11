230dc47400e06b4e0cfd9aca9df61b9860e2d9c0
Lawrence <lawrence@tealseed.com>
2024-11-11 09:42:40 +0800

    feat: init vuepress proj

---
+++ docs/.vuepress/.temp/markdown-tab/config.js
@@ -0,0 +1,10 @@
+import { CodeTabs } from "/Users/lawrence/Development/1code/journie-profile-site/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.60_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_h2k3jav3v4s4tmt75atxqsy6qm/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import { Tabs } from "/Users/lawrence/Development/1code/journie-profile-site/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.60_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_h2k3jav3v4s4tmt75atxqsy6qm/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "/Users/lawrence/Development/1code/journie-profile-site/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.60_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_h2k3jav3v4s4tmt75atxqsy6qm/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+
+export default {
+  enhance: ({ app }) => {
+    app.component("CodeTabs", CodeTabs);
+    app.component("Tabs", Tabs);
+  },
+};
---
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -0,0 +1,12 @@
+import "/Users/lawrence/Development/1code/journie-profile-site/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_l2mqyqphex6psyesb2yxtzfwhi/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "/Users/lawrence/Development/1code/journie-profile-site/node_modules/.pnpm/@vuepress+plugin-prismjs@2.0.0-rc.60_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_@vu_f3bg3qpg7tiz7khq5euy3sqnoq/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "/Users/lawrence/Development/1code/journie-profile-site/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_l2mqyqphex6psyesb2yxtzfwhi/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
+import "/Users/lawrence/Development/1code/journie-profile-site/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_l2mqyqphex6psyesb2yxtzfwhi/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
+import "/Users/lawrence/Development/1code/journie-profile-site/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_l2mqyqphex6psyesb2yxtzfwhi/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
+import { setupCollapsedLines } from "/Users/lawrence/Development/1code/journie-profile-site/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_l2mqyqphex6psyesb2yxtzfwhi/node_modules/@vuepress/highlighter-helper/lib/client/composables/collapsedLines.js"
+
+export default {
+  setup() {
+    setupCollapsedLines()
+  }
+}
